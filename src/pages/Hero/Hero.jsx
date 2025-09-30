61791cd113c27433fad39491f79b94ffa3d37793
IsmailHusun19 <ismailhusun2002>
2025-09-30 15:55:14 +0700

    main

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -47,7 +47,6 @@ export default function Hero() {
     "Experienced with React, Express & MySQL",
     "UI/UX Aware Developer",
   ];
-  
 
   const [code] = useState(`
 const profile = {
@@ -212,8 +211,9 @@ const profile = {
               {/* Description */}
               <div className="relative mb-8 sm:mb-12 max-w-xl">
                 <p className="text-base sm:text-xl text-gray-300/90 leading-relaxed">
-                Full-Stack Developer | Penggemar JavaScript | Developer React
-Bersemangat dalam membangun aplikasi web 🚀 | Merancang sistem yang dapat dikembangkan & terus belajar setiap hari 💻✨
+                  Full-Stack Developer | Penggemar JavaScript | Developer React
+                  Bersemangat dalam membangun aplikasi web 🚀 | Merancang sistem
+                  yang dapat dikembangkan & terus belajar setiap hari 💻✨
                 </p>
               </div>
 
@@ -221,7 +221,7 @@ Bersemangat dalam membangun aplikasi web 🚀 | Merancang sistem yang dapat dike
               <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 animate__animated animate__fadeInUp animate__delay-2s">
                 {/* View Projects Button */}
                 <a
-                  href="https://github.com/seraprogrammer"
+                  href="https://github.com/IsmailHusun19"
                   className="group relative inline-flex items-center justify-center gap-3 bg-gradient-to-r from-blue-500 to-teal-400 p-0.5 rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
                 >
                   <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-blue-500 group-hover:to-teal-400">
@@ -234,7 +234,9 @@ Bersemangat dalam membangun aplikasi web 🚀 | Merancang sistem yang dapat dike
 
                 {/* Contact Button */}
                 <a
-                  href="#"
+                  href="/CV - Ismail Husun.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
                 >
                   <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 border border-gray-700/50 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-gray-800 group-hover:to-gray-700">
